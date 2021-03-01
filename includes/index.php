c37a603ad578c54b07f40bd95b6648483a78d917
lee <leroy_rus@hotmail.com>
2021-03-01 13:59:14 +0000

    first commit

---
+++ includes/index.php
@@ -0,0 +1,56 @@
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Featured Post -->
+						<article id="cafes" class="post featured">
+							<header class="major">
+								<h2><span>Featured</span>
+								Cafes</h2>
+
+							</header>
+							<div class="w3-row-padding">
+								<div class="w3-col s12 m12 l4 w3-padding 8">
+									
+									<p>London is one of the most vibrant cities in England. The many attractions will surely keep you entertained but findng somewhere to eat can sometimes be a bit tricky. Look no further as we have provided a brief look at some of the best places to eat in London.</p>
+								</div>
+								<div class="w3-col s12 m12 l8">
+									<!-- <div class="w3-row-padding" id="cafeList">
+									</div> -->
+									
+									<div id="cafeCarousel" class="carousel slide w3-border" data-ride="carousel">
+										<!-- Indicators -->
+										<ol class="carousel-indicators" id="indicators">
+											
+										</ol>
+
+										<!-- Wrapper for slides -->
+										<div class="carousel-inner">
+											<div id="cafeList"></div>
+										</div>
+
+										<!-- Left and right controls -->
+										<a class="left carousel-control" href="#cafeCarousel" role="button" data-slide="prev">
+										<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+										<span class="sr-only">Previous</span>
+										</a>
+										<a class="right carousel-control" href="#cafeCarousel" role="button" data-slide="next">
+										<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+										<span class="sr-only">Next</span>
+										</a>
+									</div>
+								</div>
+								
+							</div>
+						</article>
+						
+						
+						<!-- Featured Post -->
+						<article id="map" class="post featured">
+							<header class="major">
+								<h2><span>View</span>
+								in map</h2>
+
+							</header>
+							<div id="map-canvas"></div>
+						</article>
+					</div>
